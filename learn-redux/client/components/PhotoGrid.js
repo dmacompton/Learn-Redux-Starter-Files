8d7cc51fa1783a9252330cc96be9b36776c5244d
dmacompton <d.mihaylov@ringostat.com>
2016-05-22 23:48:18 +0300

    video 11 end

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        i'm grid
+        {this.props.posts.map((post, index) => <Photo {...this.props} key={index} i={index} post={post} />)}
       </div>
     )
   }
